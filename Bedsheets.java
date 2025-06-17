ad64b316bc16873b9dbd5d4d054b4f4e5ff2dca3
yashu12374 <yashukallesh46@gmail.com>
2025-06-17 17:58:32 +0530

    sop

---
+++ Bedsheets.java
@@ -0,0 +1,10 @@
+class Bedsheets {
+public static void main(String Cotton[]){
+//message
+System.out.println("Martial: Cotton, 250 GSM TC-160");
+System.out.println("Large size to make it easy to tuck-in below the bed, Light Weight and easy to wash at Home");
+System.out.println("SuperSoft Premium Brushed Glace Cotton bedsheet, Breathable & Wrinkle Free");
+System.out.println("BSB HOME Cotton Flat Double Bedsheets with 2 King Size Pillow Covers | Double Bed Breathable | Wrinklefree | UltraSoft 200 TC Supersoft |90 x 100 Inches, 7X7.5 Feet | Green Leaf");
+System.out.println("Breathable Long Lasting Light Weight Wrinkel Free");
+}
+}
---
+++ DiningTable.java
@@ -0,0 +1,10 @@
+class DiningTable{
+public static void main(String Wood[]){
+//message
+System.out.println("Product Dimensions(In Inch): Table: Length 45 X Width 30 X Height 30 | Chair: Length 17 X Width 17 X Height 35");
+System.out.println("Premium Sheesham Wood Construction Crafted from high-quality solid wood dining table set of 4, this durable and sturdy table ensures long-lasting performance");
+System.out.println("The natural wood finish enhances its aesthetic appeal, making it a stylish dining table 4 seater for any home, hotel, or restaurant");
+System.out.println("It serves as a perfect dining solution for living rooms, dining halls, and commercial spaces or a dining table 4 seater under 15000 that fits within your budget");
+System.out.println(" This dining table set is easy to assemble, with clear instructions included, ensuring a hassle-free setup for your dining area");
+}
+}
