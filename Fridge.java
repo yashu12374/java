c23bb5289c1447e08b76dadb1a61bcf7891b8613
yashu12374 <yashukallesh46@gmail.com>
2025-06-17 14:29:05 +0530

    sop

---
+++ Fridge.java
@@ -0,0 +1,10 @@
+class Fridge{
+public static void main(String Whirlpool[]){
+//message
+System.out.println("Single door 5 Star refrigerator with Intellisense Inverter Technology");
+System.out.println("Single Door 5 Star Capacity: 184 liters for families with 2 -3 members");
+System.out.println("1 year product warranty and 10 years on compressor");
+System.out.println("Intellisense Inverter technology - Comes with low starting voltage of 95V & assures 25 years of compressor reliability- certified by VDE Germany *As Certified by VDE Germany");
+System.out.println("Storage & Interior Description - Fresh food capacity : 169.2 Ltr | Freezer capacity : 14.3 Ltr");
+}
+}
