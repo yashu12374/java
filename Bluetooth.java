17284425057f7a3a9df710fb636618948ad3fe61
yashu12374 <yashukallesh46@gmail.com>
2025-06-17 17:23:29 +0530

    sop

---
+++ Bluetooth.java
@@ -0,0 +1,10 @@
+class Bluetooth {
+public static void main (String Black []){
+//message
+System.out.println("Memory foam + Sweatproof fabric Earcups, Fit and comfort");
+System.out.println("Headphones with built-in 500mAh battery can continually play more than 50 hours");
+System.out.println("Playback-Stay connected to your playlist for mammoth durations, with up to 50 hours of playtime, and 400 Hours Standby time");
+System.out.println("Drivers-Exhilarate your senses with crystal clear sound reproduction via 45mm drivers, delivering you immersive experience");
+System.out.println("With Removable Earmuffs,fashionable design, and unique Volume + and - buttons");
+}
+}
